03f5051483a5a1d02b3688a7f066c29fc14feb74
Amanda Xavier <amandavxavier@gmail.com>
2024-01-29 23:31:31 -0300

    Feat: database tweet model

---
+++ src/main/java/com/tweteroo/api/models/TweetModel.java
@@ -0,0 +1,24 @@
+package com.tweteroo.api.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Entity
+@Table(name = "tweets")
+public class TweetModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 280, nullable = false)
+    private String text;
+}
